b4b4ab92c59c7e4ce7b286ced487bc7ea1f3aa88
user <user@FACE-8>
2019-10-04 12:32:38 +0530

    ha

--- src/main/java/main/SampleProject.java
+++ src/main/java/main/SampleProject.java
@@ -11,7 +11,7 @@ public class SampleProject {
 		int c = a+b;
 		return c;
 	}
-	//ddxfd
+	//d
 	public static void main(String args[]) {
 		System.out.println(printHello());
 		System.out.println(add(23,45));
